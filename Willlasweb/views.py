4d7181cc387a6defe69c069c290e7dae2c1c607f
Josh Nichols <jnichols20091@gmail.com>
2022-12-26 00:02:51 -0600

    stylesheets finally working

--- Willlasweb/views.py
+++ Willlasweb/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views import View
+
+from Willlasweb import *
+
 
 # Create your views here.
 def print(request):
-    return HttpResponse("Hello World")
+    return render(request, 'index.html')
