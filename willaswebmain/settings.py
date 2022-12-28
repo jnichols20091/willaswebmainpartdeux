18c750cce468f69049ba3375811b30820450a0cf
Josh Nichols <jnichols20091@gmail.com>
2022-12-28 01:17:11 -0600

    stylings with bootstrap

--- willaswebmain/settings.py
+++ willaswebmain/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/boot/css'),
+    os.path.join(BASE_DIR, 'static/boot/js'),
+    os.path.join(BASE_DIR, 'static/css'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
